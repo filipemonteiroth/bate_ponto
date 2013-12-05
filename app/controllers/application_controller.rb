85f8c5dde8bef55cca596c8ecb2aa33ec9908dbf
Filipe Monteiro <filipe.monteiro@m2msolutions.com.br>
2013-12-05 17:01:21 -0300

    Adicionando bootstrap na aplicação

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  layout :layout
+
   before_filter :authenticate_user!
 
   rescue_from CanCan::AccessDenied do |exception|
@@ -14,4 +16,9 @@ class ApplicationController < ActionController::Base
     method = "#{resource}_params"
     params[resource] &&= send(method) if respond_to?(method, true)
   end
+
+  private
+    def layout
+      devise_controller? ? "login" : "application"
+    end
 end
