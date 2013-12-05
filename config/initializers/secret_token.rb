b0b658a7cdf3adf8e5889c22cd5c2f6320a52662
Filipe Monteiro <filipe.monteiro@m2msolutions.com.br>
2013-12-05 14:55:13 -0300

    Configuração inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BatePonto::Application.config.secret_key_base = 'c7c110a6fab7c21d8c3273e36750425d674393051312e4269cb8b7b51a89922a49c6b3e64dedb1f46190379563ea05a3eaeeaa6949bc5142b9da67451a4c8cea'
